45bce1d6c9ade642c7e4b44ab9ff6f63fe9a7793
Rafael Gutiérrez <ragutimar@gmail.com>
2017-06-19 18:55:37 +0200

    Apply changes from CNabuMessagingModuleManagerAdapter

--- CNabuMailProviderManager.php
+++ CNabuMailProviderManager.php
@@ -22,7 +22,6 @@ use nabu\core\CNabuEngine;
 use nabu\core\interfaces\INabuApplication;
 use nabu\messaging\descriptors\CNabuMessagingTemplateRenderInterfaceDescriptor;
 use nabu\messaging\adapters\CNabuMessagingModuleManagerAdapter;
-use nabu\messaging\exceptions\ENabuMessagingException;
 
 /**
  * Class to manage nabu-mail library
@@ -51,7 +50,9 @@ class CNabuMailProviderManager extends CNabuMessagingModuleManagerAdapter
         $this->nb_messaging_template_render_descriptor = new CNabuMessagingTemplateRenderInterfaceDescriptor(
             $this,
             'NabuMailTemplateRender',
-            'Nabu Mail Template Render'
+            'Nabu Mail Template Render',
+            __NAMESPACE__,
+            'CNabuMailTemplateBasicRenderInterface'
         );
 
         $nb_engine->registerProviderInterface($this->nb_messaging_template_render_descriptor);
@@ -62,32 +63,5 @@ class CNabuMailProviderManager extends CNabuMessagingModuleManagerAdapter
     public function registerApplication(INabuApplication $nb_application)
     {
         return $this;
-    }
-
-    public function createServiceInterface(string $name)
-    {
-    }
-
-    public function createTemplateRenderInterface(string $name)
-    {
-        $nb_engine = CNabuEngine::getEngine();
-        $fullname = __NAMESPACE__ . "\\templates\\renders\\$name";
-
-        if ($nb_engine->preloadClass($fullname)) {
-            $interface = new $fullname($this);
-            if ($this->registerTemplateRenderInterface($interface)) {
-                return $interface;
-            } else {
-                throw new ENabuMessagingException(
-                    ENabuMessagingException::ERROR_TEMPLATE_RENDER_CANNOT_BE_INSTANTIATED,
-                    array($name)
-                );
-            }
-        } else {
-            throw new ENabuMessagingException(
-                ENabuMessagingException::ERROR_INVALID_TEMPLATE_RENDER_CLASS_NAME,
-                array($name)
-            );
-        }
     }
 }
